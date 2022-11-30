377bd240aaf842c12f7bf7b8c11193f9348a2589
Thom Dickson <td3of4@gmail.com>
2022-11-30 01:14:34 -0500

    feat: add blank bin file generation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,49 @@
+use std::{fs::File, io::Write, path::PathBuf};
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(author, version, about)]
+struct Cli {
+    #[clap(short, long, action, value_parser)]
+    /// Generate a file for another day's problems
+    generate: Option<String>,
+}
+
 fn main() {
-    println!("
+    let cli = Cli::parse();
+
+    let bins_folder = PathBuf::from("src/bin/");
+
+    if let Some(fname) = cli.generate {
+        let mut fpath = bins_folder.join(fname);
+        fpath.set_extension("rs");
+        if !fpath.exists() {
+            let mut f = File::create(&fpath).expect("Failed to create new file");
+            f.write_all(
+                format!(
+                    "fn main() {{
+    println!(\"Hello from {}!\");
+}}
+",
+                    fpath.file_stem().unwrap().to_str().unwrap().to_owned()
+                )
+                .as_bytes(),
+            )
+            .expect("Failed to write to new file");
+            println!("Generated new file at {:?}", fpath);
+        } else {
+            println!("File already exists!");
+        }
+    } else {
+        println!(
+            "
 =================================
 ||     Advent of Code 2022     ||
 || Solutions from Thom Dickson ||
 =================================
 Each day's work can be compliled and run with cargo using the --bin flag.
-For example, to run day one's code: cargo run --bin day01");
+For example, to run day one's code: cargo run --bin day01"
+        );
+    }
 }
