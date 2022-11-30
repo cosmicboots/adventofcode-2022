5ef8b37277545d65149b97c454d93b2daa042004
Thom Dickson <td3of4@gmail.com>
2022-11-30 00:44:15 -0500

    init repo

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("
+=================================
+||     Advent of Code 2022     ||
+|| Solutions from Thom Dickson ||
+=================================
+Each day's work can be compliled and run with cargo using the --bin flag.
+For example, to run day one's code: cargo run --bin day01");
+}
