392db8fc393187c7ba69c5911472c6a7d2b2d635
Thom Dickson <td3of4@gmail.com>
2022-12-05 10:22:52 -0500

    finish day05

--- src/bin/day05/main.rs
+++ src/bin/day05/main.rs
@@ -1,6 +1,26 @@
 use std::fs::read_to_string;
 
+#[derive(Clone)]
 struct Stacks(Vec<Vec<char>>);
+
+#[derive(Debug)]
+struct Move {
+    count: usize,
+    from: usize,
+    to: usize,
+}
+
+impl From<&str> for Move {
+    fn from(raw_move: &str) -> Self {
+        // move 1 from 2 to 1
+        let s: Vec<&str> = raw_move.split_ascii_whitespace().collect();
+        Self {
+            count: s[1].parse::<usize>().unwrap(),
+            from: s[3].parse::<usize>().unwrap(),
+            to: s[5].parse::<usize>().unwrap(),
+        }
+    }
+}
 
 impl From<&str> for Stacks {
     fn from(raw_stacks: &str) -> Self {
@@ -19,7 +39,7 @@ impl From<&str> for Stacks {
 
         let mut stacks: Vec<Vec<char>> = vec![vec![]; cols];
 
-        for line in lines[..lines.len() - 1].iter() {
+        for line in lines[..lines.len() - 1].iter().rev() {
             for col in 0..cols {
                 let i: usize = col * 4 + 1;
                 if i < line.len() {
@@ -35,13 +55,56 @@ impl From<&str> for Stacks {
     }
 }
 
+impl Stacks {
+    fn make_stack_moves(&mut self, moves: &Vec<Move>) {
+        for moveq in moves {
+            for _ in 0..moveq.count {
+                let val = self.0[moveq.from - 1].pop().unwrap();
+                self.0[moveq.to - 1].push(val);
+            }
+        }
+    }
+
+    fn make_moves(&mut self, moves: &Vec<Move>) {
+        for moveq in moves {
+            let f = moveq.from - 1;
+            let t = moveq.to - 1;
+            let crates = self.0[f][self.0[f].len() - moveq.count..].to_vec();
+            let truncate = self.0[f].len()-crates.len();
+            self.0[f].truncate(truncate);
+            self.0[t].extend_from_slice(&crates);
+        }
+    }
+}
+
 fn main() {
     println!("Hello from day05!");
 
-    let text = read_to_string("src/bin/day05/example.txt").unwrap();
+    let text = read_to_string("src/bin/day05/input.txt").unwrap();
 
     let (stacks_raw, moves) = text.split_once("\n\n").unwrap();
 
-    let stacks = Stacks::from(stacks_raw);
-    println!("Stacks: {:?}", stacks.0)
+    let moves = moves
+        .trim()
+        .split("\n")
+        .map(|e| Move::from(e.trim()))
+        .collect::<Vec<Move>>();
+
+    let mut stacks = Stacks::from(stacks_raw);
+    let mut stacks2 = stacks.clone();
+    println!("Starting Stacks: {:?}", stacks.0);
+    stacks.make_stack_moves(&moves);
+    stacks2.make_moves(&moves);
+    println!("Ending Stacks (part1): {:?}", stacks.0);
+    print!("Top of stacks: ");
+    for stack in stacks.0 {
+        print!("{}", stack.last().unwrap());
+    }
+    println!("\nPart2:");
+    println!("Ending Stacks (part2): {:?}", stacks2.0);
+    print!("Top of stacks: ");
+    for stack in stacks2.0 {
+        print!("{}", stack.last().unwrap());
+    }
+    println!();
 }
